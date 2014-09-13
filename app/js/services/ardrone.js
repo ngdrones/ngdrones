9152506bc7e79df45e507e0a5577250a5c8d8ad1
Rorschach22 <Ryan_mac22@hotmail.com>
2014-09-13 16:42:59 -0500

    Added all commands to service.

--- app/js/services/ardrone.js
+++ app/js/services/ardrone.js
@@ -4,7 +4,7 @@
 ==================================================================*/
 /*global app*/
 
-app.service('ardrone', ['$rootScope', '$resource', function ($rootScope, $resource) {
+app.service('ardrone', ['$rootScope', '$http', '$resource', function ($rootScope, $http, $resource) {
 
 	'use strict';
 	var _this,
@@ -15,30 +15,30 @@ app.service('ardrone', ['$rootScope', '$resource', function ($rootScope, $resour
 
 
 
-
+	var speed = 0.1;
 
 
 	ardrone = {
 		stop: function () {
-		
+			$http.get('/api/robots/ngdrone/devices/drone/commands/stop');
 		},
 		takeOff: function () {
-		
+			$http.get('/api/robots/ngdrone/devices/drone/commands/takeoff');
 		},
 		land: function () {
-		
+			$http.get('/api/robots/ngdrone/devices/drone/commands/land');
 		},
 		up: function () {
-
+			$http.post('/api/robots/ngdrone/devices/drone/commands/up', {speed: speed} );
 		},
 		down: function () {
-
+			$http.post('/api/robots/ngdrone/devices/drone/commands/down', {speed: speed} );
 		},
 		left: function () {
-
+			$http.post('/api/robots/ngdrone/devices/drone/commands/left', {speed: speed} );
 		},
 		right: function () {
-
+			$http.post('/api/robots/ngdrone/devices/drone/commands/right', {speed: speed} );
 		}
 	};
 

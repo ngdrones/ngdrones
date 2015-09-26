2a36050f5d4901b491883519e152590798fad567
Benjamin Kitt <bkitt@spscommerce.com>
2015-09-26 13:30:37 -0500

    Add server/api proxying to browsersync.

--- gulp/analyze.js
+++ gulp/analyze.js
@@ -54,5 +54,5 @@ module.exports = function (gulp, $, config) {
     });
   });
 
-  gulp.task('analyze', ['lint', 'staticAnalysis']);
+  gulp.task('analyze', ['staticAnalysis']);
 };
--- gulp/watch.js
+++ gulp/watch.js
@@ -1,13 +1,21 @@
 'use strict';
 
+var proxyMiddleware = require('http-proxy-middleware');
+
 module.exports = function (gulp, $, config) {
+  var options = {
+      target: 'http://localhost:8000/'
+  };
+  var proxy = proxyMiddleware('/api', options);
+
   gulp.task('browserSync', function () {
     $.browserSync({
       host: config.host,
       open: 'external',
       port: config.port,
       server: {
-        baseDir: config.buildDir
+        baseDir: config.buildDir,
+        middleware: [proxy]
       }
     });
   });
